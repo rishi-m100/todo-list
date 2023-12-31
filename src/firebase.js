4cd7c2e85287756de03efdf9be4d095949ccbe0b
Rishi Mulchandani <rishimulchandani100@gmail.com>
2023-12-31 03:42:50 -0500

    all

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyDf26ie4a8V_9Fy7_UrlOJoTOZrRbbtwm8",
+  authDomain: "todo-list-35f2a.firebaseapp.com",
+  databaseURL: "https://todo-list-35f2a-default-rtdb.firebaseio.com",
+  projectId: "todo-list-35f2a",
+  storageBucket: "todo-list-35f2a.appspot.com",
+  messagingSenderId: "456802960158",
+  appId: "1:456802960158:web:6c0b1b5f77374b49903f0f",
+});
+
+export { firebaseConfig as firebase };
