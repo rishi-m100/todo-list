43ddc0a3b66dd2a5ce8ac94e5e9ca86735a73f45
Rishi Mulchandani <rishimulchandani100@gmail.com>
2024-01-03 03:24:48 -0500

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 const firebaseConfig = firebase.initializeApp({
   apiKey: "AIzaSyDf26ie4a8V_9Fy7_UrlOJoTOZrRbbtwm8",
